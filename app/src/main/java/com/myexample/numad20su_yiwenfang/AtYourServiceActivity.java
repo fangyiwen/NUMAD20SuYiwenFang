29368dc1b00307cb84bd5826868384129545149f
Yiwen Fang <fang.yiw@husky.neu.edu>
2020-06-19 17:15:56 -0700

    Create indication for web service connection

--- app/src/main/java/com/myexample/numad20su_yiwenfang/AtYourServiceActivity.java
+++ app/src/main/java/com/myexample/numad20su_yiwenfang/AtYourServiceActivity.java
@@ -21,7 +21,7 @@ import java.util.Scanner;
 
 public class AtYourServiceActivity extends AppCompatActivity {
 
-    private TextView textView11;
+    private TextView textView10, textView11;
     private ApiTask myTask;
     private String pending = "(Pending...)", inputPostCode;
 
@@ -30,6 +30,7 @@ public class AtYourServiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_at_your_service);
 
+        textView10 = findViewById(R.id.textView10);
         textView11 = findViewById(R.id.textView11);
     }
 
@@ -51,20 +52,41 @@ public class AtYourServiceActivity extends AppCompatActivity {
         protected String doInBackground(String... params) {
             String result;
             try {
+                // Update web service connection status
+                publishProgress("Web Service Connection Status: Start connection");
+
                 URL url = new URL(params[0]);
+                publishProgress("Web Service Connection Status: Connecting... (URL is OK)");
 
                 // Start HttpURLConnection
                 HttpURLConnection connect = (HttpURLConnection) url.openConnection();
                 connect.setRequestMethod("GET");
                 connect.setDoInput(true);
-                connect.connect();
+
+                // Check the API service still works
+                try {
+                    connect.connect();
+                } catch (IOException e) {
+                    publishProgress("Web Service Connection Status: Disconnected");
+                    result = "\n\nError: IOException (API service is not available)";
+                    return result;
+                }
+                publishProgress("Web Service Connection Status: Connection is successful");
 
                 InputStream inputStream;
-                inputStream = connect.getInputStream();
+                // Check InputStream is successful for the post code request
+                try {
+                    inputStream = connect.getInputStream();
+                } catch (IOException e) {
+                    publishProgress("Web Service Connection Status: Disconnected");
+                    result = "\n\nError: IOException (No information for the post code)";
+                    return result;
+                }
 
                 // Deal with response
                 Scanner myScanner = new Scanner(inputStream).useDelimiter("\\A");
                 String response = myScanner.hasNext() ? myScanner.next() : "";
+                publishProgress("Web Service Connection Status: Disconnected (Data is retrieved from API)");
 
                 // Deal with JSON response format
                 JSONObject myJsonObject = new JSONObject(response);
@@ -88,17 +110,22 @@ public class AtYourServiceActivity extends AppCompatActivity {
                         + "Geo Coordinates:\n"
                         + "Latitude: " + latitude + "\n"
                         + "Longitude: " + longitude;
+                publishProgress("Web Service Connection Status: Disconnected");
                 return result;
             } catch (MalformedURLException e) {
+                publishProgress("Web Service Connection Status: Disconnected");
                 result = "\n\nError: MalformedURLException";
                 return result;
             } catch (ProtocolException e) {
+                publishProgress("Web Service Connection Status: Disconnected");
                 result = "\n\nError: ProtocolException";
                 return result;
             } catch (IOException e) {
+                publishProgress("Web Service Connection Status: Disconnected");
                 result = "\n\nError: IOException";
                 return result;
             } catch (JSONException e) {
+                publishProgress("Web Service Connection Status: Disconnected");
                 result = "\n\nError: JSONException";
                 return result;
             }
@@ -107,6 +134,7 @@ public class AtYourServiceActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(String... values) {
             // Update and show web service connection status
+            textView10.setText(values[0]);
         }
 
         @Override
